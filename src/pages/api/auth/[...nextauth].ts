72cc9550d5f24eaf3716159b1179f44940964281
Afif Abdillah Jusuf <bungambohlah@gmail.com>
2022-08-13 15:51:50 +0700

    refactor: :recycle: only enable facebook provider for authentication of login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,8 @@
 import NextAuth, { type NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+// import GoogleProvider from 'next-auth/providers/google';
 import FacebookProvider from 'next-auth/providers/facebook';
-import GithubProvider from 'next-auth/providers/github';
-import TwitterProvider from 'next-auth/providers/twitter';
+// import GithubProvider from 'next-auth/providers/github';
+// import TwitterProvider from 'next-auth/providers/twitter';
 
 // Prisma adapter for NextAuth, optional and can be removed
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
@@ -16,18 +16,18 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.FACEBOOK_ID || '',
       clientSecret: process.env.FACEBOOK_SECRET || '',
     }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID || '',
-      clientSecret: process.env.GITHUB_SECRET || '',
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID || '',
-      clientSecret: process.env.GOOGLE_SECRET || '',
-    }),
-    TwitterProvider({
-      clientId: process.env.TWITTER_ID || '',
-      clientSecret: process.env.TWITTER_SECRET || '',
-    }),
+    // GithubProvider({
+    //   clientId: process.env.GITHUB_ID || '',
+    //   clientSecret: process.env.GITHUB_SECRET || '',
+    // }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_ID || '',
+    //   clientSecret: process.env.GOOGLE_SECRET || '',
+    // }),
+    // TwitterProvider({
+    //   clientId: process.env.TWITTER_ID || '',
+    //   clientSecret: process.env.TWITTER_SECRET || '',
+    // }),
   ],
   callbacks: {
     session({ session, user }) {
